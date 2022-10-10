691d7ea44e8b6f3adcd360782bba3317ca20da0e
VitaliiTsono <104579923+VitaliiTsono@users.noreply.github.com>
2022-10-10 20:56:31 +0300

    Update UserServiceImpl.java
    Done userService

--- src/main/java/com/softserve/itacademy/service/impl/UserServiceImpl.java
+++ src/main/java/com/softserve/itacademy/service/impl/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.softserve.itacademy.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
 
@@ -19,25 +21,76 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User addUser(User user) {
-        // TODO
-        return null;
+
+        if (nullCheck(user)) throw new RuntimeException(user + " has a null parameter");
+
+        boolean exist = users.stream()
+                .filter(user1 -> user1.getEmail().equals(user.getEmail()))
+                .count() == 1 ? true : false;
+
+
+        if(!isValidEmailAddress(user.getEmail()))
+            throw new RuntimeException(user.getEmail() + " - is invalid");
+
+        if (exist){
+            throw new RuntimeException(user.getEmail()+" - user already exist");
+        }else {
+            users.add(user);
+            return user;
+        }
     }
 
     @Override
     public User updateUser(User user) {
-        // TODO
-        return null;
+        if (nullCheck(user)) throw new RuntimeException(user + " has a null parameter");
+
+        User userOriginal = users.stream()
+                .filter(user1 -> user1.getEmail().equals(user.getEmail()))
+                .findFirst().orElse(null);
+
+        if (userOriginal == null){
+            throw new RuntimeException(user.getEmail()+" - user not found");
+        }else {
+            users.set(users.indexOf(userOriginal), user);
+            return user;
+        }
+
+
     }
 
     @Override
     public void deleteUser(User user) {
-        // TODO
+        if (nullCheck(user)) throw new RuntimeException(user + " has a null parameter");
+
+        if (users.indexOf(user)==-1)
+            throw new RuntimeException(user.getEmail()+" - user not found");
+        else
+            users.remove(user);
     }
 
     @Override
     public List<User> getAll() {
-        // TODO
-        return null;
+        return users;
+    }
+
+
+
+    private boolean isValidEmailAddress(String email) {
+        Pattern pattern =
+                Pattern.compile("^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$");
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    private boolean nullCheck(User user){
+        if (user == null
+                || user.getEmail()==null || user.getEmail().isEmpty()
+                || user.getPassword()==null || user.getPassword().isEmpty()
+                || user.getFirstName()==null || user.getFirstName().isEmpty()
+                || user.getLastName()==null || user.getLastName().isEmpty()
+        ) return true;
+
+        return false;
     }
 
 }
