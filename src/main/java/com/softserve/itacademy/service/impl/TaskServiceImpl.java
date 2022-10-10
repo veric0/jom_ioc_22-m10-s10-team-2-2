f19bd8e0b9655cacc6685bec05a072be1e205eec
VitaliiTsono <Sailor00V@gmail.com>
2022-10-10 20:46:59 +0300

    TaskService done

--- src/main/java/com/softserve/itacademy/service/impl/TaskServiceImpl.java
+++ src/main/java/com/softserve/itacademy/service/impl/TaskServiceImpl.java
@@ -1,6 +1,8 @@
 package com.softserve.itacademy.service.impl;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,37 +24,64 @@ public class TaskServiceImpl implements TaskService {
     }
 
     public Task addTask(Task task, ToDo todo) {
-        // TODO
-        return null;
+        List<Task> tasks = todo.getTasks();
+        if (tasks.contains(task)){
+            throw new RuntimeException("Task exist already");
+        } else {
+            tasks.add(task);
+        }
+        return task;
     }
 
     public Task updateTask(Task task) {
-        // TODO
-        return null;
+        List<Task> allTasks = getAll();
+        Task updTask;
+        if (allTasks.contains(task)) {
+            Optional<Task> optionalTask = allTasks.stream()
+                    .filter(task1 -> task1.equals(task)).findFirst();
+            updTask = optionalTask.get();
+            updTask.setPriority(task.getPriority());
+        } else {
+            throw new RuntimeException("Tasks not found");
+        }
+        return updTask;
     }
 
     public void deleteTask(Task task) {
-        // TODO
+        List<Task> allTasks = getAll();
+        if (allTasks.contains(task)) {
+            allTasks.remove(task);
+        } else {
+            throw new RuntimeException("Task not found");
+        }
     }
 
     public List<Task> getAll() {
-        // TODO
-        return null;
+        return toDoService.getAll().stream()
+                .flatMap(toDo -> toDo.getTasks().stream())
+                .collect(Collectors.toList());
     }
 
     public List<Task> getByToDo(ToDo todo) {
-        // TODO
-        return null;
+        return toDoService.getAll().stream()
+                .filter(toDo1 -> toDo1.equals(todo))
+                .flatMap(toDo -> toDo.getTasks().stream())
+                .collect(Collectors.toList());
     }
 
     public Task getByToDoName(ToDo todo, String name) {
-        // TODO
-        return null;
+        return getByToDo(todo).stream()
+                .filter(task -> task.getName().equals(name))
+                .findFirst()
+                .get();
     }
 
     public Task getByUserName(User user, String name) {
-        // TODO
-        return null;
+        return toDoService.getByUser(user).stream()
+                .flatMap(toDo -> toDo.getTasks().stream())
+                .filter(task -> task.getName().equals(name))
+                .findFirst()
+                .get();
     }
 
 }
