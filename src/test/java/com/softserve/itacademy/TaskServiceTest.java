3152a1ed5f9f66b971b92abf961b8efd8d218509
ArtemKachur <armkachur@gmail.com>
2022-10-12 12:10:07 +0300

    edited class TaskServiceTest 1

--- src/test/java/com/softserve/itacademy/TaskServiceTest.java
+++ src/test/java/com/softserve/itacademy/TaskServiceTest.java
@@ -1,5 +1,9 @@
 package com.softserve.itacademy;
 
+import com.softserve.itacademy.model.Priority;
+import com.softserve.itacademy.model.Task;
+import com.softserve.itacademy.model.ToDo;
+import com.softserve.itacademy.service.TaskService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -10,21 +14,50 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import com.softserve.itacademy.model.User;
 import com.softserve.itacademy.service.UserService;
 
+import java.time.LocalDateTime;
+
 @RunWith(JUnitPlatform.class)
 public class TaskServiceTest {
     private static UserService userService;
+
+    private static TaskService taskService;
+
+
+    private static User user;
+    private static ToDo todo;
 
     @BeforeAll
     public static void setupBeforeClass() throws Exception {
         AnnotationConfigApplicationContext annotationConfigContext = new AnnotationConfigApplicationContext(Config.class);
         userService = annotationConfigContext.getBean(UserService.class);
+        taskService = annotationConfigContext.getBean(TaskService.class);
+
+        user = new User("Naomi","Kembel",
+                "Naomi1987@gmail.com","GhfjUJHK");
+
+        todo = new ToDo(user.getFirstName()+"ToDo", LocalDateTime.now(), user);
+
         annotationConfigContext.close();
     }
 
     @Test
     public void checkAddTask() {
-        // TODO, code
+        Task actual = taskService.addTask(new Task("Cheese cl", Priority.HIGH),
+                todo);
+        Task expected = new Task("Cheese cl", Priority.HIGH);
+        Assertions.assertEquals(actual,expected,"addTask can not work");
     }
 
-    // TODO, other tests
+    @Test
+    public void checkAddExistTask() {
+        Task task = new Task("Cheese cl", Priority.HIGH);
+        boolean thrown = false;
+        try {
+            taskService.addTask(task,todo);
+        }catch (RuntimeException tae){
+            thrown=true;
+        }
+        Assertions.assertTrue(thrown,"task - already exist");
+    }
+
 }
